67b6a1612fbae16588fbb7c3758316474c2177f5
Abhishek Bhatia <abhibhatia2000@gmail.com>
2024-08-19 02:01:33 +0530

    FIX: Error Handling

--- Express/contacts_backend/middleware/error_handler.js
+++ Express/contacts_backend/middleware/error_handler.js
@@ -1,9 +1,10 @@
-const { constants } = require("../constants.js")
+const { constants } = require("../constants.js");
 
 const errorHandler = (err, req, res, next) => {
     const statusCode = res.statusCode ? res.statusCode : 500;
     console.log(statusCode);
-    const title = (() => {
+    
+    const errTitle = (() => {
         switch (statusCode) {
             case constants.VALIDATION_ERROR:
                 return "Validation Error";
@@ -12,14 +13,15 @@ const errorHandler = (err, req, res, next) => {
             case constants.UNAUTHORIZED_ERROR:
                 return "Unauthorized";
             case constants.FORBIDDEN_ERROR:
-                return "Forbidden !";
+                return "Forbidden!";
             case constants.INTERNAL_SERVER_ERROR:
                 return "Internal Server Error";
             default:
                 return "Something went wrong";
         }
-    });
-    res.json({ title: title, message: err.message, stackTrace: err.stack });
+    })(); // <-- Invoke the function immediately
+
+    res.status(statusCode).json({ title: errTitle, message: err.message, stackTrace: err.stack });
 };
 
 module.exports = errorHandler;
